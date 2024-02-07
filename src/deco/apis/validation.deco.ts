ad36a0ee0621c86a635beba57f6fb31668242423
elierosie0 <elierosie0@gmail.com>
2024-02-07 14:27:59 +0700

    modifies

--- src/deco/apis/validation.deco.ts
+++ src/deco/apis/validation.deco.ts
@@ -1,5 +1,5 @@
 import { Store } from '../../services/store.service'
-import { MetadataKeys, NonSafe } from '../../utils/types'
+import { MetadataKeys } from '../../utils/types'
 
 /**
  * Validation middleware. A function which is called before the route handler.
@@ -7,10 +7,10 @@ import { MetadataKeys, NonSafe } from '../../utils/types'
  * @param schema schema object.
  * @returns
  */
-export function Validation(schema: NonSafe): MethodDecorator {
+export function Validation<T = unknown>(schema: T): MethodDecorator {
     return (Target, propertyKey) => {
         // define a new metadata object and set it up in the container Store.
-        Store.container.define<NonSafe>(
+        Store.container.define<T>(
             Target.constructor.prototype,
             schema,
             MetadataKeys.__api_method_validation__,
